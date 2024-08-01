d7ca1e9d38fc823b93e19066c1c6018f561658b0
David Rodríguez <deivid.rodriguez@riseup.net>
2024-08-01 21:54:06 +0200

    Remove DOCKER_USER

--- docker/manager.rb
+++ docker/manager.rb
@@ -125,7 +125,7 @@ module Docker
           docker
           login
           -u
-          #{ENV['DOCKER_USER']}
+          deividrodriguez
           -p
           #{ENV['DOCKER_PASS']}
         ]
